cdfb7ca77d99392bb0c742ae8f41b9e1bff9a6f8
TaranjotSingh10 <taranjot10934@gmail.com>
2026-02-24 14:14:17 +0530

    Update model pickle file and other changes for Streamlit app

--- web_app.py
+++ web_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Heart_Disease_prediction", page_icon=":shark:", l
 st.title("Heart Disease Prediction")
 
 def load_model():
-    return pickle.load(open("heart.pkl", "rb"))
+    return pickle.load(open("heart (1).pkl", "rb"))
 
 value1 = st.number_input("Age", min_value=1, max_value=120, value=30)
 value2 = st.selectbox("Sex (0 = Female, 1 = Male)", [0, 1])
